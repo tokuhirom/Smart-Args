c6a5ca9041debff9bbe7317dc34015ec4d7851ae
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-08-23 23:40:46 +0900

    initial commit

---
+++ lib/args.pm
@@ -0,0 +1,33 @@
+package args;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+args -
+
+=head1 SYNOPSIS
+
+  use args;
+
+=head1 DESCRIPTION
+
+args is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
